9dc74f925873fd81f303c1002ab87a49ef683284
Roman Zeno <zeno385@gmail.com>
2025-07-17 22:08:25 +0300

    countdown timer

--- formatTime.js
+++ formatTime.js
@@ -1,18 +1,22 @@
-// function pad(num, digit = 2) {
-//   return num.toString().padStart(digit, "0");
-// }
+export function pad(num, digit = 2) {
+  return num.toString().padStart(digit, "0");
+}
 
-// function formatTime(ms) {
-//   const milliseconds = ms % 1000;
-//   const totalSeconds = Math.floor(ms / 1000);
-//   const seconds = totalSeconds % 60;
-//   const minutes = Math.floor(totalSeconds / 60) % 60;
-//   const hours = Math.floor(totalSeconds / 3600);
+export function formatTime(ms) {
+  const milliseconds = ms % 1000;
+  const totalSeconds = Math.floor(ms / 1000);
+  const seconds = totalSeconds % 60;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const hours = Math.floor(totalSeconds / 3600);
 
-//   return `${pad(hours)}:${pad(minutes)}:${pad(seconds)}.${pad(
-//     milliseconds,
-//     3
-//   )}`;
-// }
+  return `${pad(hours)}:${pad(minutes)}:${pad(seconds)}.${pad(
+    milliseconds,
+    3
+  )}`;
+}
 
+// const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+// const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+// const minutes = Math.floor((diff / (1000 * 60)) % 60);
+// const seconds = Math.floor((diff / 1000) % 60);
 // // console.log("formatTime(2000) :>> ", formatTime(2000000111)); // 00:00:02.000
--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import { formatTime, pad } from "./formatTime.js";
+
 const timeDisplay = document.querySelector("#time");
 const startBtn = document.querySelector("#startBtn");
 const pauseBtn = document.querySelector("#pauseBtn");
@@ -12,34 +14,38 @@ startBtn.addEventListener("click", start);
 pauseBtn.addEventListener("click", pause);
 resetBtn.addEventListener("click", reset);
 
-function pad(num, digit = 2) {
-  return num.toString().padStart(digit, "0");
-}
+// function pad(num, digit = 2) {
+//   return num.toString().padStart(digit, "0");
+// }
 
-function formatTime(ms) {
-  const milliseconds = ms % 1000;
-  const totalSeconds = Math.floor(ms / 1000);
-  const seconds = totalSeconds % 60;
-  const minutes = Math.floor(totalSeconds / 60) % 60;
-  const hours = Math.floor(totalSeconds / 3600);
+// function formatTime(ms) {
+//   const milliseconds = ms % 1000;
+//   const totalSeconds = Math.floor(ms / 1000);
+//   const seconds = totalSeconds % 60;
+//   const minutes = Math.floor(totalSeconds / 60) % 60;
+//   const hours = Math.floor(totalSeconds / 3600);
 
-  return `${pad(hours)}:${pad(minutes)}:${pad(seconds)}.${pad(milliseconds)}`;
-}
+//   return `${pad(hours)}:${pad(minutes)}:${pad(seconds)}.${pad(milliseconds)}`;
+// }
 
 function updateTime() {
   const now = Date.now();
   elapsedTime = now - startTime;
-  console.log("elapsedTime :>> ", elapsedTime);
+  // console.log("elapsedTime :>> ", elapsedTime);
   timeDisplay.textContent = formatTime(elapsedTime);
 }
 // console.log("Date.now() :>> ", Date.now());
 
 function start() {
-  //   console.log("start :>> ");
-  startTime = Date.now() - elapsedTime;
-  intervalId = setInterval(updateTime, 10);
-  //   console.log("startTime :>> ", startTime);
-  isRuning = true;
+  if (!isRuning) {
+    //   console.log("start :>> ");
+    startTime = Date.now() - elapsedTime;
+    intervalId = setInterval(updateTime, 10);
+    console.log("intervalId :>> ", intervalId);
+    //   console.log("startTime :>> ", startTime);
+    isRuning = true;
+    return;
+  }
 }
 function pause() {
   //   console.log("pause :>> ");
@@ -53,5 +59,67 @@ function reset() {
   clearInterval(intervalId);
   elapsedTime = 0;
   isRuning = false;
-  timeDisplay.textContent = "00:00:00.00";
+  timeDisplay.textContent = "00:00:00.000";
 }
+
+//6 ==================== countdown timer =============================
+
+const dateTimeInput = document.querySelector("#datetime");
+const startBtnCoutndown = document.querySelector("#startTimer");
+const countdownDisplay = document.querySelector("#countdown");
+const countdownDisplayMsg = document.querySelector("#message");
+
+let countdownIntervalId;
+
+startBtnCoutndown.addEventListener("click", startCountdown);
+
+function startCountdown() {
+  countdownDisplayMsg.textContent = "";
+  const targetDate = new Date(dateTimeInput.value); // я вводив не вірно Date.now
+  const now = new Date();
+  if (isNaN(targetDate.getTime())) {
+    countdownDisplayMsg.textContent = "❌ Введіть коректну дату!";
+    return;
+  } else if (targetDate <= now) {
+    countdownDisplayMsg.textContent = "❌ Оберіть дату майбутню!";
+    return;
+  }
+
+  clearInterval(countdownIntervalId);
+
+  countdownIntervalId = setInterval(() => {
+    const currentTime = new Date();
+    const diffTime = targetDate.getTime() - currentTime;
+    // console.log("targetDate :>> ", targetDate);
+    // console.log("now :>> ", now);
+    // console.log("diffTime :>> ", diffTime);
+    if (diffTime <= 0) {
+      clearInterval(countdownIntervalId);
+      countdownDisplayMsg.textContent = "✅ Час настав!";
+      // countdownDisplay.textContent = "00d 00h 00min 00s";
+      return;
+    }
+
+    const days = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((diffTime / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((diffTime / (1000 * 60)) % 60);
+    const seconds = Math.floor((diffTime / 1000) % 60);
+
+    countdownDisplay.innerHTML =
+      // countdownDisplay.textContent =
+      `${pad(days)}<small>d</small> ${pad(hours)}<small>h</small> ${pad(
+        minutes
+      )}<small>min</small> ${pad(seconds)}<small>sec</small>`;
+  }, 1000);
+}
+
+// const resetBtnCountdown = document.querySelector("#resetTimer");
+
+// resetBtnCountdown.addEventListener("click", resetCountdown);
+
+// function resetCountdown() {
+//   clearInterval(countdownIntervalId);
+//   countdownDisplay.textContent = "00d 00h 00min 00s";
+//   countdownDisplayMsg.textContent = "";
+//   dateTimeInput.value = "";
+// }
